ffac7de67a6e3e3b277dd385196cdbf128453203
Won Kim <wonsae.kim@gmail.com>
2019-06-04 15:47:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    let formData = {
+        name: userName,
+        email: userEmail
+    }
+
+    let configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+    return fetch('http://localhost:3000/users', configObj)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(function(obj) {
+            let para = document.querySelector('body');
+            let innerPoop = document.createElement('p');
+            innerPoop.innerHTML = obj["id"]
+            para.appendChild(innerPoop);
+        })
+        .catch(function(error) {
+            let para = document.querySelector('body')
+            let innerPoop = document.createElement('p');
+            innerPoop.innerHTML = error
+            para.appendChild(innerPoop);
+        })
+}
