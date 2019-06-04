86a7a58ef326ffd5ba001989ec2837005cd0e286
Jason Gomez <jason.gomez@me.com>
2019-06-04 15:41:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+const submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    }).then((res) => {
+        return res.json();
+    }).then((json) => {
+        document.body.innerHTML = json['id'];
+    }).catch((error) => {
+        document.body.innerHTML = error.message;
+    });
+}
