92cd95e67ca8041ea2af21a9f0cec2539d16b1b8
henryk1229 <henryk1229@gmail.com>
2019-06-05 18:00:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+
+  let body_select = document.querySelector("#body_id")
+
+  let user = {
+    name: `${userName}`,
+    email: `${userEmail}`
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(user)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      var p = document.createElement('p')
+      p.innerHTML = object['id']
+      body_select.appendChild(p);
+    })
+    .catch(function(error) {
+      var p = document.createElement('p')
+      p.innerHTML = error['message']
+      body_select.appendChild(p)
+    });
+}
