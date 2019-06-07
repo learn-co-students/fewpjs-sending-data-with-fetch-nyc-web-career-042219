548df5f59c900e4d793c437ac94ebf9de0c745a3
Adam Sultanov <adam.sultanov@gmail.com>
2019-06-07 09:22:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData (name, email) {
+
+  let body = document.querySelector("body")
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  }
+
+return fetch("http://localhost:3000/users", configObj)
+    .then( res => res.json() )
+    .then( json => {
+      body.innerHTML = json.id
+    })
+    .catch( error => {
+      body.innerHTML = error.message
+    })
+}
