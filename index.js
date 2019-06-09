f62d84164970fefce55cc96bba67f2d5e4ea5212
cbatsoula <cbatsoula@gmail.com>
2019-06-09 16:20:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,60 +1,6 @@
-// fetch('http://localhost:3000/dogs', configurationObject);
-// configurationObject = {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json";
-//     "Accept": "application/json";
-//   },
-//   body: JSON.stringify({
-//     dogName: "Byron",
-//     dogBreed: "Poodle"
-//   })
-// };
-//
 
-// fetch("http://localhost:3000/dogs", {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Accept": "application/json"
-//   },
-//   body: JSON.stringify({
-//     dogName: "Byron",
-//     dogBreed: "Poodle"
-//   })
-// });
-// //
-//
-//
-// let formData = {
-//   dogName: "Byron",
-//   dogBreed: "Poodle"
-// };
-//
-// let configObj = {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Accept": "application/json"
-//   },
-//   body: JSON.stringify(formData)
-// };
-
-// fetch("http://localhost:3000/dogs", configObj)
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(object) {
-//     console.log(object);
-//   })
-//   .catch(function(error) {
-//     alert("Bad things! Ragnarők!");
-//     console.log(error.message);
-//   });
-
-
-  function submitData( name, email ) {
-  return fetch( 'http://localhost:3000/users', {
+  function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -63,15 +9,15 @@
       body: JSON.stringify( {
         name,
         email
-      } )
-    } )
-    .then( function ( response ) {
+      })
+    })
+    .then(function (response) {
       return response.json()
-    } )
-    .then( function ( object ) {
-      document.body.innerHTML = object[ "id" ]
-    } )
-    .catch( function ( error ) {
+    })
+    .then(function (object) {
+      document.body.innerHTML = object["id"]
+    })
+    .catch(function (error) {
       document.body.innerHTML = error.message
-    } )
+    })
 }
